63b5964642e214d4d4f0ad00c06b74540367a6d0
edulop <elopez@chanzuckerberg.com>
2018-09-21 07:47:53 -0700

    stuff

--- pkg/ssh/token_provider.go
+++ pkg/ssh/token_provider.go
@@ -10,7 +10,7 @@ import (
 // TokenProvider lets us simulate some interactivity during
 // ssh flow
 // HACK HACK: This probably deserves a longer explanation.
-func TokenProvider(prompt string) (string, error) {
+func TokenProvider(prompt string) func() (string, error) {
 	// While running through the ssh config (match exec, ProxyCommand, etc)
 	// ssh commandeers stdin and stdout for internal use.
 	// this makes it hard to do things like asking for an MFA token.
@@ -18,10 +18,12 @@ func TokenProvider(prompt string) (string, error) {
 	// and is used to communicate with the user.
 	// We exploit this quirk to provide a simple interactive mode
 	// that allows us to request MFA tokens from users
-	newStdIn := os.NewFile(uintptr(syscall.Stderr), "/dev/stdin")
-	defer newStdIn.Close()
-	reader := bufio.NewReader(newStdIn)
-	text, err := reader.ReadString('\n')
-	text = strings.TrimSuffix(text, "\n")
-	return text, err
+	return func() (string, error) {
+		newStdIn := os.NewFile(uintptr(syscall.Stderr), "/dev/stdin")
+		defer newStdIn.Close()
+		reader := bufio.NewReader(newStdIn)
+		text, err := reader.ReadString('\n')
+		text = strings.TrimSuffix(text, "\n")
+		return text, err
+	}
 }
