b7e11520d8aa8e3d7b0f47feb5ae4499fc72702a
Vivian Ho <vho@lyft.com>
2018-11-15 11:26:11 -0800

    Updated comment

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -84,7 +84,7 @@ type LambdaConfig struct {
 	RoleARN string `yaml:"role_arn"`
 	// Bless lambda function name
 	FunctionName string `yaml:"function_name"`
-	// Bless lambda function version
+	// Bless lambda function version (lambda alias or version qualifier)
 	FunctionVersion *string `yaml:"function_version,omitempty"`
 	// bless lambda regions
 	Regions []Region `yaml:"regions,omitempty"`
